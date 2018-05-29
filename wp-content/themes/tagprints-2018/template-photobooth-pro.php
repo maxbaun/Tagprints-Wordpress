78025b6a14b19e800fc403b8d35e4c900add92f9
Max Baun <max@d3applications.com>
2018-05-29 15:55:02 -0400

    adding photobooth pro template

---
+++ wp-content/themes/tagprints-2018/template-photobooth-pro.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Photobooth Pro
+ *
+ * @package - TagPrints 2018 Theme
+ */
+
+?>
+
+<?php get_template_part('templates/includes/head'); ?>
+
+<main class="main" role="main"></main>
+
+<?php get_template_part('templates/includes/foot'); ?>
+
