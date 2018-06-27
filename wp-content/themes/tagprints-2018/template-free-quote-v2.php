0a1560cc5f74fb3927f3f1208314d9af22d31943
Max Baun <max@d3applications.com>
2018-06-26 20:02:01 -0400

    adding 3 new template files

---
+++ wp-content/themes/tagprints-2018/template-free-quote-v2.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: Free Quote V2
+ *
+ * @package - TagPrints 2018 Theme
+ */
+
+?>
+
+<?php get_template_part('templates/includes/head'); ?>
+
+<main class="main" role="main"></main>
+
+<?php get_template_part('templates/includes/foot'); ?>
