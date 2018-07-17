4000a583cbbbf8a4f9fedf46cbf1d290bb99e77a
Max Baun <max@d3applications.com>
2018-07-17 13:02:38 -0400

    adding thanks template

---
+++ wp-content/themes/tagprints-2018/template-thanks.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Template Name: Thanks Template
+*/
+?>
+
+<?php get_template_part('templates/includes/head'); ?>
+
+<main class="main" role="main"></main>
+
+<?php get_template_part('templates/includes/foot'); ?>
