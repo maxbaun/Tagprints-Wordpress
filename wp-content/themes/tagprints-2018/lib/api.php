85156af1da8b955fd7e9342a9b9b24dfc366bb63
Max Baun <max@d3applications.com>
2018-08-01 18:08:40 -0400

    adding gatsby return values for acf

--- wp-content/themes/tagprints-2018/lib/api.php
+++ wp-content/themes/tagprints-2018/lib/api.php
@@ -63,26 +63,98 @@ add_filter('rest_endpoints', function ($endpoints) {
 	return $endpoints;
 });
 
-add_filter( 'rest_post_collection_params', 'tagprints_prefix_add_rest_orderby_params', 10, 1 );
-add_filter( 'rest_lookbook_collection_params', 'tagprints_prefix_add_rest_orderby_params', 10, 1 );
-add_filter( 'rest_case_study_collection_params', 'tagprints_prefix_add_rest_orderby_params', 10, 1 );
+add_filter('rest_post_collection_params', 'tagprints_prefix_add_rest_orderby_params', 10, 1);
+add_filter('rest_lookbook_collection_params', 'tagprints_prefix_add_rest_orderby_params', 10, 1);
+add_filter('rest_case_study_collection_params', 'tagprints_prefix_add_rest_orderby_params', 10, 1);
 
 function tagprints_prefix_add_rest_orderby_params($params) {
-    $params['orderby']['enum'][] = 'menu_order';
+	$params['orderby']['enum'][] = 'menu_order';
 
-    return $params;
+	return $params;
 }
 
 function add_menu_order_to_return() {
 	register_rest_field(
-	  array('case_study'), // add to these post types
-	  'menu_order', // name of field
-	  array(
-		'get_callback' => function ($post) {
-		  return intval(get_post_field('menu_order', $post['id'])); // value of field
-		}
-	  )
+		array('case_study'), // add to these post types
+		'menu_order', // name of field
+		array('get_callback' => function ($post) {
+			return intval(get_post_field('menu_order', $post['id'])); // value of field
+		})
 	);
-  }
+}
 
-  add_action('rest_api_init', 'add_menu_order_to_return');
+add_action('rest_api_init', 'add_menu_order_to_return');
+
+
+add_filter('acf/format_value/type=image', function ($value, $post_id, $field) {
+	if (empty($value)) {
+		return null;
+	}
+
+	return $value;
+}, 100, 3);
+
+add_filter('acf/format_value/type=relationship', function ($value, $post_id, $field) {
+	if (empty($value)) {
+		return null;
+	}
+
+	$newValues = array();
+	foreach ($value as $v) {
+		$v->featured_media = wp_get_attachment_image_url(get_post_thumbnail_id($v->ID), 'full');
+		$v->url = get_permalink($v);
+		$newValues[] = $v;
+	}
+
+	return $newValues;
+}, 100, 3);
+// add_filter('acf/format_value/type=relationship', 'nullify_empty', 100, 3);
+// // not sure if gallery is internally named gallery as well but this should work
+// add_filter('acf/format_value/type=gallery', 'nullify_empty', 100, 3);
+add_filter('acf/format_value/type=repeater', function ($value, $post_id, $field) {
+	if (empty($value)) {
+		return null;
+	}
+
+	return $value;
+}, 100, 3);
+
+add_filter('acf/format_value/type=group', function ($value, $post_id, $field) {
+	if (empty($value)) {
+		return null;
+	}
+
+	return $value;
+}, 100, 3);
+
+add_filter('acf/format_value/type=component_field', function ($value, $post_id, $field) {
+	if (empty($value)) {
+		return null;
+	}
+
+	return $value;
+}, 100, 3);
+
+add_filter('acf/format_value/type=flexible_content', function ($value, $post_id, $field) {
+	if (empty($value)) {
+		return array();
+	}
+
+	return $value;
+}, 100, 3);
+
+add_filter('acf/format_value/type=image', function ($value, $post_id, $field) {
+	if (empty($value)) {
+		return null;
+	}
+
+	return $value;
+}, 100, 3);
+
+add_filter('acf/format_value/type=link', function ($value, $post_id, $field) {
+	if (empty($value)) {
+		return null;
+	}
+
+	return $value;
+}, 100, 3);
