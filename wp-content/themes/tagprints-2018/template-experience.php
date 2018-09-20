fd9785fdf05d37c7c9f7b0837caef66a298d93c8
Max Baun <max@d3applications.com>
2018-09-20 15:01:08 -0400

    adding experience template

---
+++ wp-content/themes/tagprints-2018/template-experience.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: Experience
+ *
+ * @package - TagPrints 2018 Theme
+ */
+
+?>
+
+<?php get_template_part('templates/includes/head'); ?>
+
+<main class="main" role="main"></main>
+
+<?php get_template_part('templates/includes/foot'); ?>
